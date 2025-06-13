734407bc6f6604e1c7afb922fb585b0da10f5a11
M. Andres Jurado <anmijurane@gmail.com>
2025-06-13 12:43:24 -0600

    LOGS: add logs for debbuging

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { locationsGym } from "./data/location_gym";
 const isProd = import.meta.env.PROD;
 
 const ASSET_EXTENSIONS = [
-  '.js', '.css', '.json', '.png', '.jpg', '.jpeg', 
+  '.js', '.css', '.json', '.png', '.jpg', '.jpeg',
   '.svg', '.gif', '.webp', '.ico', '.woff', '.woff2', '.map'
 ];
 
@@ -13,7 +13,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
   // --- FILTRO DE ASSETS ---
   // Si es un asset, no ejecutamos nada de la lógica de abajo.
   if (
-    url.pathname.startsWith('/_astro/') || 
+    url.pathname.startsWith('/_astro/') ||
     ASSET_EXTENSIONS.some(ext => url.pathname.endsWith(ext))
   ) {
     return next();
@@ -21,10 +21,12 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
   const slug = url.searchParams.get('slug');
   const alreadyCookieExist = cookies.get('slug')?.value;
+  console.log('_____________');
   console.log({ slug, alreadyCookieExist });
   // 1. Validamos si viene un slug en la URL
   if (slug) {
     const isValidGym = locationsGym.find(gym => gym.slug === slug && gym.active);
+    console.log('Acceso por:', { gym: isValidGym?.club });
     if (isValidGym) {
       cookies.set('slug', slug, {
         path: '/',
@@ -33,7 +35,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
         secure: isProd,
         expires: new Date(Date.now() + 1000 * 60 * 60 * 12),
       });
-      return next();
+      return next('/');
     }
   }
 
