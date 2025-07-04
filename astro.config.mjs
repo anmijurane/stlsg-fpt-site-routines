9cb3d77f5d01f7bec5b6489034fbd76c4d6043e5
M. Andres Jurado <anmijurane@gmail.com>
2025-07-03 23:51:23 -0600

    FEAT: Add dotenv configuration to astro.config and ensure dotenv dependency is included in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
