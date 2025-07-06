18f2fbd576bcb281f6822cc6deb6030854ce4c7e
M. Andres Jurado <anmijurane@gmail.com>
2025-07-06 17:48:43 -0600

    FEAT: Add interfaces for StateExercise, ColdAndHeat, and StateRoutines; implement routines actions for exercise management

---
+++ src/store/routines.actions.ts
@@ -0,0 +1,40 @@
+import { routines } from "@data/index"
+import type { TypeRoutine } from "@interfaces/routines"
+import type { ColdAndHeat, StateExercise, StateRoutines } from "@interfaces/store"
+import { Routine } from "@store/routines"
+
+export const setDoneExerciseById = (dayId: number, props: Omit<StateExercise, 'name'>) => {
+  const routines = Routine.get()[dayId];
+  const newExercise = routines.exercises.map(exercise => ({ ...exercise, isDone: props.id === exercise.id ? props.isDone : exercise.isDone }));
+  Routine.setKey(`${dayId}`, { ...routines, exercises: newExercise })
+}
+
+export const setDoneExerciseCoolOrHeat = (dayId: number, props: Omit<StateExercise, 'name'>) => {
+  const routines = Routine.get()[dayId];
+  Routine.setKey(`${dayId}`, { ...routines })
+}
+
+export const initialStateByRoutine = (category: TypeRoutine, level: number) => {
+  const { levels } = routines[category];
+  const currentLevel = levels.find((it) => it.id === level);
+
+  console.log(currentLevel);
+
+  if (!currentLevel) { console.log('No valid level'); return }
+
+  const routine: Record<string, StateRoutines> = {};
+
+  currentLevel.days.forEach(day => {
+    routine[day.id] = {
+      id: day.id,
+      exercises: day.exercises.map(exercise => ({ id: exercise.id, isDone: false, name: exercise.name })),
+      name: day.name,
+      coolHeat: day.exercises.length >= 2 ? [
+        { id: 'heating', name: 'heating', isDone: false },
+        { id: 'cooling', name: 'cooling', isDone: false }
+      ] : [],
+    }
+  })
+  console.log(routine);
+  Routine.set(routine);
+}
---
+++ src/store/routines.ts
@@ -0,0 +1,4 @@
+import type { StateRoutines } from '@interfaces/store';
+import { map } from 'nanostores';
+
+export const Routine = map<Record<string, StateRoutines>>({});
