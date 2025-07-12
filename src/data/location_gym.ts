ffb3be066667e60d0fa6ab4b03a2e7dd844574fc
M. Andres Jurado <anmijurane@gmail.com>
2025-07-11 20:12:54 -0600

    MOD: sort gym data

--- src/data/location_gym.ts
+++ src/data/location_gym.ts
@@ -1,8 +1,17 @@
 export const locationsGym = [
-  { "slug": "a912a94d79", "state": "Stadistica",  "club": "statistics",             "active": true  },
-  { "slug": "6623005224", "state": "Queretaro",   "club": "Cimatario",              "active": true  },
+  { "slug": "a912a94d79", "state": "Stadistica",  "club": "statistics",             "active": true  }, // Only test
+  { "slug": "6623005224", "state": "Queretaro",   "club": "Cimatario",              "active": true  }, // MVP 1
+  { "slug": "fa3dde34d7", "state": "Monterrey",   "club": "El Molino",              "active": true  }, // MVP 1
+  { "slug": "236a7d40af", "state": "Guadalajara", "club": "Punto Rio Nilo",         "active": true  }, // MVP 1
+  { "slug": "90e4b5c13a", "state": "San Luis",    "club": "The Park",               "active": true  }, // MVP 1
+
+  { "slug": "6f1c8b2375", "state": "Leon",        "club": "Punto León",             "active": true  }, // MVP 2
+  { "slug": "c022aa7cdc", "state": "Queretaro",   "club": "Candiles",               "active": true  }, // MVP 2
+  { "slug": "2e7d9a4c05", "state": "San Luis",    "club": "Glorieta",               "active": true  }, // MVP 2
+  { "slug": "8c1029a5e7", "state": "Queretaro",   "club": "Antea",                  "active": true  }, // MVP 2
+  { "slug": "f1e2d3c4b5", "state": "Guadalajara", "club": "Plaza Del Angel",        "active": true  }, // MVP 2
+
   { "slug": "c4e220d3af", "state": "Puebla",      "club": "Torrecillas",            "active": false },
-  { "slug": "c022aa7cdc", "state": "Queretaro",   "club": "Candiles",               "active": true  },
   { "slug": "aa85485a04", "state": "Cuernavaca",  "club": "Galerías Cuernavaca",    "active": false },
   { "slug": "6a2052d8f9", "state": "CDMX",        "club": "Paseo Acoxpa",           "active": false },
   { "slug": "9e4f35b27c", "state": "Edo de Mex",  "club": "Satelite",               "active": false },
@@ -17,23 +26,16 @@ export const locationsGym = [
   { "slug": "17cf8bbf29", "state": "Monterrey",   "club": "Santa Catarina",         "active": false },
   { "slug": "660e5a1d8c", "state": "Monterrey",   "club": "La Estanzuela",          "active": false },
   { "slug": "c6232e9461", "state": "Monterrey",   "club": "Arcadia",                "active": false },
-  { "slug": "fa3dde34d7", "state": "Monterrey",   "club": "El Molino",              "active": true  },
   { "slug": "0b0986e177", "state": "Saltillo",    "club": "Santa Isabel",           "active": false },
   { "slug": "5dc0a1b409", "state": "Puebla",      "club": "Angelópolis",            "active": false },
   { "slug": "7aa8f6d5c3", "state": "Edo de Mex",  "club": "Coacalco",               "active": false },
-  { "slug": "236a7d40af", "state": "Guadalajara", "club": "Punto Rio Nilo",         "active": true  },
   { "slug": "15bd2f6108", "state": "CDMX",        "club": "Acueducto",              "active": false },
-  { "slug": "90e4b5c13a", "state": "San Luis",    "club": "The Park",               "active": true  },
   { "slug": "b70f4e2d6a", "state": "VER",         "club": "Jardines Xalapa",        "active": false },
   { "slug": "257d3e6a8b", "state": "Veracruz",    "club": "Center Plazas",          "active": false },
-  { "slug": "8c1029a5e7", "state": "Queretaro",   "club": "Antea",                  "active": true  },
   { "slug": "a49d5f73c4", "state": "CDMX",        "club": "Mixcoac",                "active": false },
   { "slug": "31b8e2a04f", "state": "CDMX",        "club": "Cumbres 1188",           "active": false },
   { "slug": "3d207a9c12", "state": "Edo de Mex",  "club": "Centro Las Americas",    "active": false },
-  { "slug": "6f1c8b2375", "state": "Leon",        "club": "Punto León",             "active": true  },
   { "slug": "9b4a5e7d13", "state": "Veracruz",    "club": "Andamar",                "active": false },
   { "slug": "c50f0f52f8", "state": "Veracruz",    "club": "Los Pinos",              "active": false },
-  { "slug": "2e7d9a4c05", "state": "San Luis",    "club": "Glorieta",               "active": true  },
-  { "slug": "f1e2d3c4b5", "state": "Guadalajara", "club": "Plaza Del Angel",        "active": true  },
   { "slug": "4bfa66b080", "state": "Mazatlan",    "club": "Gran Plaza Mazatlan",    "active": false }
 ];
